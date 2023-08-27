065dcecdf2fd4d5977cab1d25e2d645cef6a75aa
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-08-27 15:04:46 +0200

    Update Phonebook.js

--- src/components/Phonebook.js
+++ src/components/Phonebook.js
@@ -1,49 +1,19 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addContact, deleteContact, updateFilter } from 'redux/phonebookSlice';
-import { PersistGate } from 'redux-persist/integration/react'; 
+import { PersistGate } from 'redux-persist/integration/react';
 import ContactForm from './ContatForm/Contactform';
 import ContactList from './Contactlist';
 import Filter from './Filter';
 import { persistor } from 'redux/store';
 
 const Phonebook = () => {
-  const contacts = useSelector(state => state.phonebook.contacts);
-  const filter = useSelector(state => state.phonebook.filter);
-  const dispatch = useDispatch();
-
-  const handleSubmit = newContact => {
-    const contactExists = contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase());
-
-    if (contactExists) {
-      alert(`${newContact.name} is already in contacts`);
-      return;
-    }
-
-    dispatch(addContact(newContact));
-  };
-
-  const handleChangeFilter = event => {
-    dispatch(updateFilter(event.target.value));
-  };
-
-  const handleDelete = id => {
-    dispatch(deleteContact(id));
-  };
-
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-     console.log('filtered', filteredContacts);
-
   return (
     <PersistGate loading={null} persistor={persistor}>
       <div>
         <h1>Phonebook</h1>
-        <ContactForm handleSubmit={handleSubmit} />
+        <ContactForm />
         <h2>Contacts</h2>
-        <Filter filter={filter} handleChangeFilter={handleChangeFilter} />
-        <ContactList contacts={filteredContacts} handleDelete={handleDelete} />
+        <Filter />
+        <ContactList />
       </div>
     </PersistGate>
   );
